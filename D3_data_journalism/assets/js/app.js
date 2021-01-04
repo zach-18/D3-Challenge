0bd575b76662abe2b40ebcbe2d86a8798b09c585
zach-18 <67443071+zach-18@users.noreply.github.com>
2021-01-03 22:24:23 -0600

    added code for scatter plot from 16.3.9

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,6 +1,31 @@
 // @TODO: YOUR CODE HERE!
+/** From 16.3.9 from Activities */
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+
 /** From 16.2 Ins_Loading_Data */
-// Load data from hours-of-tv-watched.csv
+// Load data from data.csv
 d3.csv("./data.csv").then(function(censusData) {
 
     console.log(censusData);
@@ -9,7 +34,7 @@ d3.csv("./data.csv").then(function(censusData) {
     var state = censusData.map(data => data.state);
     console.log("state", state);
   
-    // Cast each hours value in tvData as a number using the unary + operator
+    // Cast each value censusData as a number using the unary + operator
     censusData.forEach(function(data) {
       data.poverty = +data.poverty;
       data.healthcare = +data.healthcare;
@@ -17,6 +42,48 @@ d3.csv("./data.csv").then(function(censusData) {
       console.log("Poverty:", data.poverty);
       console.log("Lack Healthcare:", data.healthcare);
     });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(censusData, d => d.poverty)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(censusData, d => d.healthcare)])
+      .range([height, 0]);
+      
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
+
+
+    
+
+
   }).catch(function(error) {
     console.log(error);
   });
+
