c0452b7c85bfbd9edae6dbea9f5c3afde539b739
zach-18 <67443071+zach-18@users.noreply.github.com>
2021-01-03 21:35:48 -0600

    accessed data

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,20 @@
 // @TODO: YOUR CODE HERE!
+/** From 16.2 Ins_Loading_Data */
+// Load data from hours-of-tv-watched.csv
+d3.csv("./data.csv").then(function(censusData) {
+
+    console.log(censusData);
+  
+    // log a list of names
+    var state = censusData.map(data => data.state);
+    console.log("state", state);
+  
+    // Cast each hours value in tvData as a number using the unary + operator
+    censusData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      console.log("state:", data.state);
+      console.log("Poverty:", data.poverty);
+    });
+  }).catch(function(error) {
+    console.log(error);
+  });
